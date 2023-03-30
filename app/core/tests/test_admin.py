394dd7bfcb2ef8843dfb792d247ec5aa53ea24e2
Brian Kibiku <kibikukimani2@gmail.com>
2023-03-30 18:13:00 +0300

    add drf_spectacular to allow for api schema generation

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,7 +9,7 @@ from django.test import Client
 class AdminSiteTests(TestCase):
     """Tests for Django admin"""
 
-    def sefUp(self):
+    def setUp(self):
         """Create user and client"""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -30,3 +30,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.email)
         self.assertContains(res, self.user.name)
+
+    def test_edit_user_page(self):
+        """Test the edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
