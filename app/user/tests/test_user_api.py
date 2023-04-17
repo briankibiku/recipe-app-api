6dee970d3dfa30d842b25bb5a16efe835c90f5a9
Brian Kibiku <kibikukimani2@gmail.com>
2023-04-17 07:54:50 +0300

    add create user feat using tdd

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -43,7 +43,7 @@ class PublicUserApiTests(TestCase):
             'name': 'test name'
         }
         create_user(**payload)
-        res = self.client.request(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
