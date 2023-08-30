f8d2ba44eb805bf94a5f250caf90707e8225b780
Brian Kibiku <kibikukimani2@gmail.com>
2023-08-30 13:24:10 +0300

    add serializer and views

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,7 +9,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View to manage recipe APIs"""
-    serializer_class = serializers.Recipe
+    serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
